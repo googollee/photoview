4d0fa01692bd4b186419aa6abcc1d5b27a08c225
Googol Lee <i@googol.im>
2025-07-10 16:31:34 +0200

    Export coverage as artifact.

--- scripts/test_api_coverage.sh
+++ scripts/test_api_coverage.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 cd "$(dirname $0)/../api"
-go test ./... -v -database -filesystem -p 1 -race -coverpkg=./... -coverprofile=coverage.txt -covermode=atomic
+go test ./... -v -database -filesystem -p 1 -race -coverpkg=./... -coverprofile=coverage.txt
